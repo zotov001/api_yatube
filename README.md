52d262775a13d754a6965624f8ebf73ba9847d53
Марк Зотов <zotov001@yandex.ru>
2022-08-16 19:12:47 +0300

    var1

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
+# backend_community_homework
 # api_yatube
 api_yatube
+### Технологии
+Python 3.7
+Django 2.2.19
+### Запуск проекта в dev-режиме
+- Установите и активируйте виртуальное окружение
+- Установите зависимости из файла requirements.txt
+```
+pip install -r requirements.txt
+``` 
+- В папке с файлом manage.py выполните команду:
+```
+python3 manage.py runserver
+```
+### Автор
+Марк
--- yatube_api/yatube_api/urls.py
+++ yatube_api/yatube_api/urls.py
@@ -3,8 +3,24 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
+from rest_framework.authtoken import views
+from rest_framework.routers import DefaultRouter
+
+from posts.views import CommentViewSet, GroupViewSet, PostViewSet
+
+router = DefaultRouter()
+router.register(r'api/v1/posts', PostViewSet)
+router.register(r'api/v1/groups', GroupViewSet)
+router.register(
+    r'api/v1/posts/(?P<post_id>\d+)/comments',
+    CommentViewSet,
+    basename='comments',
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/api-token-auth/', views.obtain_auth_token),
+    path('', include(router.urls)),
 ]
 
 
